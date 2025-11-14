f36ffa79e5a56ad9c70b713259070c7de073e0e0
DSC <144414027+Dasksz@users.noreply.github.com>
2025-11-13 23:51:25 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ async function initializeNewDashboard(supabaseClient) {
         ] = await Promise.all([
             supabase.from('data_clients').select('*'),
             supabase.from('data_product_details').select('code,descricao,codfor,fornecedor,dtcadastro'),
-            supabase.from('data_innovations').select('code,description,category'),
+            supabase.from('data_innovations').select('codigo, produto, inovacoes'),
             api.getDistinctSupervisors(supabase),
             api.getDistinctFornecedores(supabase),
             api.getDistinctTiposVenda(supabase),
